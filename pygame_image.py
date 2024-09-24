4a2f4f16a956fe5aa1448c77fc0ea061e3027520
Kousei <c0a2316313@edu.teu.ac.jp>
2024-09-24 17:11:24 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -20,16 +20,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst = pg.key.get_pressed()
+        kouka = [-1,0]
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            kouka[1] += -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+            kouka[1] +=1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            kouka[0] += -1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2,0))
-        else:
-            kk_rct.move_ip((-1,0))
+            kouka[0] += 2
+        kk_rct.move_ip(kouka)
         x = -(tmr%3200)
 
         screen.blit(bg_img, [x, 0])
